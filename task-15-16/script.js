ab64315dd707409c6b98ab8b5f8457a27fb23ab8
Meerim Jutankeeva <meerim.jutankeeva.kk@gmail.com>
2022-03-24 15:06:39 +0600

    16

---
+++ task-15-16/script.js
@@ -0,0 +1,61 @@
+const productsInner = document.querySelector(".products__inner");
+const productsFilters = document.querySelector(".products__filters");
+fetch("text.json")  // FETCH API
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    data.forEach((e) => {
+      let productsItem = document.createElement("article");
+      productsItem.className = "products__item";
+
+      let img = document.createElement("img");
+      img.className = "products__item-photo";
+
+      let productsItemInfo = document.createElement("div");
+      productsItemInfo.className = "products__item-info";
+
+      let header = document.createElement("header");
+
+      let h3 = document.createElement("h3");
+
+      let productsItemPrice = document.createElement("span");
+      productsItemPrice.className = "products__item-price";
+
+      let productsItemText = document.createElement("p");
+      productsItemText.className = "products__item-text";
+
+      let cards = document.createElement("div");
+      cards.classList = `cards  ${e.category}`;
+
+      productsInner.appendChild(cards);
+      cards.appendChild(productsItem);
+      productsItem.appendChild(img);
+      productsItem.appendChild(productsItemInfo);
+      productsItemInfo.appendChild(header);
+      productsItemInfo.appendChild(productsItemText);
+      header.appendChild(h3);
+      header.appendChild(productsItemPrice);
+
+      img.src = e.img;
+      h3.innerHTML = e.title;
+      productsItemPrice.textContent = e.price + "сом";
+      productsItemText.innerHTML = e.desc;
+
+      const filterCard = document.querySelectorAll(".cards");
+      productsFilters.addEventListener("click", (event) => {
+        if (event.target.tagName !== "BUTTON") return false;
+        let filterClass = event.target.dataset["filter"];
+        filterCard.forEach((e) => {
+          e.classList.remove("hide");
+          if (!e.classList.contains(filterClass) && filterClass !== "all") {
+            e.classList.add("hide");
+          }
+        });
+      });
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+  
